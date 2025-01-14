04b140d04b1028a3b81ccde8968c72c136380d9e
xdmethodz <abirxdhackz@gmail.com>
2025-01-14 14:20:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from others.downloaders import setup_downloader_handler
 from others.pin import setup_pinterest_handler
 from others.dl import setup_dl_handlers
 from others.spotify import setup_spotify_handler
-from aitools.gemi import setup_gemini_handler
+
 # Replace these with your actual API details
 API_ID = "24602058"  # Replace with your API ID
 API_HASH = "b976a44ccb8962b20113113f84aeebf6"  # Replace with your API Hash
@@ -53,7 +53,7 @@ setup_downloader_handler(app)
 setup_pinterest_handler(app)
 setup_dl_handlers(app)
 setup_spotify_handler(app)
-setup_gemini_handler(app)
+
 # Inline keyboard for the main menu
 main_menu_keyboard = InlineKeyboardMarkup([
     [
