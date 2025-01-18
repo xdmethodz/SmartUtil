94911755bc3dcba8f04e4947df05237689bf49e6
xdmethodz <abirxdhackz@gmail.com>
2025-01-18 09:24:42 +0600

    Update down.py

--- converter/down.py
+++ converter/down.py
@@ -1,6 +1,7 @@
 import os
 import re
-import requests
+import aiohttp
+import asyncio
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import zipfile
@@ -18,63 +19,75 @@ class URLDownloader:
         self.link_flg = link_flg
         self.script_flg = script_flg
         self.link_type = ('css', 'png', 'ico', 'jpg', 'jpeg', 'mov', 'ogg', 'gif', 'xml', 'js')
-        self.session = requests.Session()
         
-    def save_page(self, url, page_folder='page'):
+    async def fetch(self, session, url):
+        async with session.get(url) as response:
+            return await response.read()
+        
+    async def save_page(self, url, page_folder='page'):
         """Save the web page components based on the input URL and directory name."""
         try:
             # Ensure the URL has a scheme
             if not urlparse(url).scheme:
                 url = "https://" + url
 
-            response = self.session.get(url)
-            self.soup = BeautifulSoup(response.text, "html.parser")  # Use html.parser if lxml is not available
-            if not os.path.exists(page_folder):
-                os.mkdir(page_folder)
+            async with aiohttp.ClientSession() as session:
+                response = await self.fetch(session, url)
+                self.soup = BeautifulSoup(response, "html.parser")  # Use html.parser if lxml is not available
+                if not os.path.exists(page_folder):
+                    os.mkdir(page_folder)
                 
-            if self.img_flg:
-                self._soup_find_and_save(url, page_folder, tag_to_find='img', inner='src')
-            if self.link_flg:
-                self._soup_find_and_save(url, page_folder, tag_to_find='link', inner='href')
-            if self.script_flg:
-                self._soup_find_and_save(url, page_folder, tag_to_find='script', inner='src')
+                tasks = []
+                if self.img_flg:
+                    tasks.append(self._soup_find_and_save(session, url, page_folder, 'img', 'src'))
+                if self.link_flg:
+                    tasks.append(self._soup_find_and_save(session, url, page_folder, 'link', 'href'))
+                if self.script_flg:
+                    tasks.append(self._soup_find_and_save(session, url, page_folder, 'script', 'src'))
                 
-            with open(os.path.join(page_folder, 'page.html'), 'wb') as file:
-                file.write(self.soup.prettify('utf-8'))
+                await asyncio.gather(*tasks)
                 
-            self._zip_folder(page_folder, url)
-            return True
+                with open(os.path.join(page_folder, 'page.html'), 'wb') as file:
+                    file.write(self.soup.prettify('utf-8'))
+                
+                self._zip_folder(page_folder, url)
+                return True
         except Exception as e:
             print(f"> save_page(): Create files failed: {str(e)}")
             return False
 
-    def _soup_find_and_save(self, url, page_folder, tag_to_find='img', inner='src'):
+    async def _soup_find_and_save(self, session, url, page_folder, tag_to_find='img', inner='src'):
         """Save specified tag_to_find objects in the page_folder."""
         page_folder = os.path.join(page_folder, tag_to_find)
         if not os.path.exists(page_folder):
             os.mkdir(page_folder)
-            
+        
+        tasks = []
         for res in self.soup.findAll(tag_to_find):
-            try:
-                if not res.has_attr(inner):
-                    continue  # Check if inner tag (file object) exists
+            if res.has_attr(inner):
+                tasks.append(self._download_resource(session, url, res, page_folder, inner))
                 
-                filename = re.sub(r'\W+', '.', os.path.basename(res[inner]))
-                if tag_to_find == 'link' and (not any(ext in filename for ext in self.link_type)):
-                    filename += '.html'
-                    
-                file_url = urljoin(url, res.get(inner))
-                file_path = os.path.join(page_folder, filename)
-                
-                res[inner] = os.path.join(os.path.basename(page_folder), filename)
-                if not os.path.isfile(file_path):
+        await asyncio.gather(*tasks)
+    
+    async def _download_resource(self, session, url, res, page_folder, inner):
+        """Download and save a resource."""
+        try:
+            filename = re.sub(r'\W+', '.', os.path.basename(res[inner]))
+            if tag_to_find == 'link' and (not any(ext in filename for ext in self.link_type)):
+                filename += '.html'
+
+            file_url = urljoin(url, res.get(inner))
+            file_path = os.path.join(page_folder, filename)
+
+            res[inner] = os.path.join(os.path.basename(page_folder), filename)
+            if not os.path.isfile(file_path):
+                content = await self.fetch(session, file_url)
+                if content:
                     with open(file_path, 'wb') as file:
-                        file_bin = self.session.get(file_url)
-                        if len(file_bin.content) > 0:
-                            file.write(file_bin.content)
-            except Exception as exc:
-                print(exc, file=sys.stderr)
-                
+                        file.write(content)
+        except Exception as exc:
+            print(exc)
+
     def _zip_folder(self, folder_path, url):
         """Zip the folder."""
         sanitized_url = re.sub(r'\W+', '_', url)
@@ -108,7 +121,7 @@ async def download_web_source(client: Client, message: Message):
         # Download the webpage components
         downloader = URLDownloader()
         page_folder = os.path.join("downloads", urlparse(url).netloc)
-        if downloader.save_page(url, page_folder):
+        if await downloader.save_page(url, page_folder):
             zip_path = downloader._zip_folder(page_folder, url)
 
             # Send the zip file to the user
