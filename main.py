bc566b63910d4fdedc8cf753c39ca791b32ec337
xdmethodz <abirxdhackz@gmail.com>
2025-01-26 00:16:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from converter.ss import setup_ss_handler
 from converter.quote import setup_q_handler
 from converter.git import setup_git_handler
 from stringsession.string import setup_string_handler
-
+from others.reboot import setup_reboot_handler
 # Replace these with your actual API details
 API_ID = "24602058"  # Replace with your API ID
 API_HASH = "b976a44ccb8962b20113113f84aeebf6"  # Replace with your API Hash
@@ -81,7 +81,7 @@ setup_ss_handler(app)
 setup_q_handler(app)
 setup_git_handler(app)
 setup_string_handler(app)
-
+setup_reboot_handler(app)
 @app.on_message(filters.command("start") & filters.private)
 async def send_start_message(client, message):
     chat_id = message.chat.id
