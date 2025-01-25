13f2501d5caddc19be1c58e329ec9c402df5c47c
xdmethodz <abirxdhackz@gmail.com>
2025-01-25 22:50:11 +0600

    Update downloaders.py

--- others/downloaders.py
+++ others/downloaders.py
@@ -18,7 +18,7 @@ YT_COOKIES_PATH = "./cookies/cookies.txt"
 
 # Configure logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name=s)s - %(levelname=s)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
