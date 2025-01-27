de7c523db07b9274558a214bc95180caa710a535
xdmethodz <abirxdhackz@gmail.com>
2025-01-27 10:18:07 +0600

    Update quote.py

--- converter/quote.py
+++ converter/quote.py
@@ -5,7 +5,7 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.enums import ParseMode
+from pyrogram.enums import ParseMode, ChatType
 
 # ThreadPoolExecutor instance
 executor = ThreadPoolExecutor(max_workers=5)  # You can adjust the number of workers
@@ -20,17 +20,16 @@ async def generate_quote(client: Client, message: Message):
         return
 
     text = " ".join(command_parts[1:])
+    
     user = message.from_user
-    full_name = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
-
-    # Get the user's profile photo
-    avatar_file_path = None
-    if user and user.photo:
-        avatar_file_path = await client.download_media(user.photo.big_file_id)
+    if user:
+        full_name = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
+        user_id = user.id
     else:
-        # If user info cannot be fetched, use group avatar and group name
-        if message.chat.type in ["group", "supergroup"]:
+        # If user info cannot be fetched, use group name and avatar
+        if message.chat.type in [ChatType.SUPERGROUP, ChatType.GROUP]:
             full_name = message.chat.title
+            user_id = message.chat.id
             if message.chat.photo:
                 avatar_file_path = await client.download_media(message.chat.photo.big_file_id)
             else:
@@ -39,6 +38,14 @@ async def generate_quote(client: Client, message: Message):
         else:
             await message.reply_text("**You do not have a profile photo set.**", parse_mode=ParseMode.MARKDOWN)
             return
+
+    # Get the user's profile photo
+    avatar_file_path = None
+    if user and user.photo:
+        avatar_file_path = await client.download_media(user.photo.big_file_id)
+    elif not avatar_file_path:
+        await message.reply_text("**You do not have a profile photo set.**", parse_mode=ParseMode.MARKDOWN)
+        return
 
     # Convert the avatar to base64
     with open(avatar_file_path, "rb") as file:
@@ -57,7 +64,7 @@ async def generate_quote(client: Client, message: Message):
                 "entities": [],
                 "avatar": True,
                 "from": {
-                    "id": user.id,
+                    "id": user_id,
                     "name": full_name,
                     "photo": {
                         "url": f"data:image/jpeg;base64,{avatar_base64}"
@@ -79,7 +86,8 @@ async def generate_quote(client: Client, message: Message):
 
     await client.send_sticker(message.chat.id, file_path)
     os.remove(file_path)  # Clean up the file
-    os.remove(avatar_file_path)  # Clean up the downloaded avatar file
+    if avatar_file_path:
+        os.remove(avatar_file_path)  # Clean up the downloaded avatar file
 
 def setup_q_handler(app: Client):
     @app.on_message(filters.command("q") & (filters.private | filters.group))
