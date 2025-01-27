7d88c0e009382ec9ad3c9f7bdee55b45850220e3
xdmethodz <abirxdhackz@gmail.com>
2025-01-27 08:46:11 +0600

    Create alive.py

---
+++ others/alive.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import ChatMemberUpdated
+from pyrogram.enums import ChatMemberStatus, ParseMode
+
+def setup_alive_handler(app: Client):
+    @app.on_chat_member_updated(filters.chat_member_updated)
+    async def welcome_on_add(client: Client, chat_member_updated: ChatMemberUpdated):
+        if chat_member_updated.new_chat_member.user.is_self and chat_member_updated.new_chat_member.status == ChatMemberStatus.MEMBER:
+            chat_id = chat_member_updated.chat.id
+            await client.send_message(
+                chat_id,
+                "**Thank You For Adding Me In This Group👨‍💻**",
+                parse_mode=ParseMode.MARKDOWN
+            )
