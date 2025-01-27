d51a82f2b6127e7abddabef6eae892098d754d0f
xdmethodz <abirxdhackz@gmail.com>
2025-01-27 08:51:15 +0600

    Update alive.py

--- others/alive.py
+++ others/alive.py
@@ -1,9 +1,9 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 from pyrogram.enums import ChatMemberStatus, ParseMode
 
 def setup_alive_handler(app: Client):
-    @app.on_chat_member_updated(filters.chat_member_updated)
+    @app.on_chat_member_updated()
     async def welcome_on_add(client: Client, chat_member_updated: ChatMemberUpdated):
         if chat_member_updated.new_chat_member.user.is_self and chat_member_updated.new_chat_member.status == ChatMemberStatus.MEMBER:
             chat_id = chat_member_updated.chat.id
